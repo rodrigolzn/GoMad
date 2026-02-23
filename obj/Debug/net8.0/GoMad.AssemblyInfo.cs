4c6ba6e45ad4f86d21a5123070fbdf05df4b15e0
Jaime Ortega <Jaimestu9@gmail.com>
2026-02-23 12:45:30 +0100

    politica de privacidad

--- obj/Debug/net8.0/GoMad.AssemblyInfo.cs
+++ obj/Debug/net8.0/GoMad.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GoMad")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b198b1529b7a0bda262d661069e3d8f5812e81ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbd4c13e4918c5048df487dd1d8fc610790fc396")]
 [assembly: System.Reflection.AssemblyProductAttribute("GoMad")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GoMad")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
